688925a43b475bd3a225b044b775a70f64d3d27c
iVesperX <aermias@live.com>
2018-09-05 10:17:58 -0400

    pushed files

---
+++ src/test/java/aermias/consuming_restful_service/ConsumingRestfulServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package aermias.consuming_restful_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumingRestfulServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
